d07e8ac981b99ab7cd5ed81be4fc540b95b7532d
Daniel octo <danielindra02@gmail.com>
2024-04-01 12:14:27 +0700

    memodifikasi file index dan tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
     extend: {
       colors: {
         'primary': '#0891b2',
@@ -9,6 +13,13 @@ module.exports = {
         'tertiary': '#155e75',
       }
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1320px',
+    }
   },
   plugins: [],
 }
