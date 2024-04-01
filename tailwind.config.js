08093acecdb03f4d32366dc23f15d4cf50b64f13
Daniel octo <danielindra02@gmail.com>
2024-04-01 10:40:30 +0700

    membuat file html dan tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0891b2',
+        'secondary': '#0f766e',
+        'tertiary': '#155e75',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
